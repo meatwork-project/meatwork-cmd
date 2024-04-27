fc1d6b0a098de19d1a1501456064208ee8f94336
dbarb <d.barbarin@hotmail.fr>
2024-04-27 12:08:26 +0200

    feat: update cmdline

--- src/main/java/com/meatwork/cmd/api/CmdLine.java
+++ src/main/java/com/meatwork/cmd/api/CmdLine.java
@@ -1,10 +1,7 @@
 package com.meatwork.cmd.api;
 
-import java.util.concurrent.Callable;
-
 /*
  * Copyright (c) 2016 Taliro.
  * All rights reserved.
  */
-public abstract class CmdLine implements Callable<Integer> {
-}
+public interface CmdLine{}
