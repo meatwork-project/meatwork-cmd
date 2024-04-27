274a3ee3bb816105ba4b4232f275d90b3dba591d
dbarb <d.barbarin@hotmail.fr>
2024-04-27 12:02:30 +0200

    feat: update sub cmd forget callable

--- src/main/java/com/meatwork/cmd/api/SubCmd.java
+++ src/main/java/com/meatwork/cmd/api/SubCmd.java
@@ -1,7 +1,9 @@
 package com.meatwork.cmd.api;
 
+import java.util.concurrent.Callable;
+
 /*
  * Copyright (c) 2016 Taliro.
  * All rights reserved.
  */
-public interface SubCmd {}
+public interface SubCmd extends Callable<Integer> {}
