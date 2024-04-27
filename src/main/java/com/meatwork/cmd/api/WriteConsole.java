babf1b9f6ada3c85b1b790e15b9e64bce3d32196
dbarb <d.barbarin@hotmail.fr>
2024-04-27 15:21:30 +0200

    feat: update encoding msg

---
+++ src/main/java/com/meatwork/cmd/api/WriteConsole.java
@@ -0,0 +1,45 @@
+package com.meatwork.cmd.api;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Copyright (c) 2016 Taliro.
+ * All rights reserved.
+ */
+public final class WriteConsole {
+
+	public void response(byte[] bytes) {
+		StringBuffer sb = new StringBuffer();
+		for (byte b : bytes) {
+			sb.append(b);
+		}
+		System.out.println(sb);
+	}
+
+	public void send(String responseEncoded) {
+		System.out.println(responseEncoded);
+	}
+
+	public String encode(String string) {
+		List<String> result = new ArrayList<>();
+		char[] chars = string.toCharArray();
+		for (char aChar : chars) {
+			result.add(
+					String.format("%8s", Integer.toBinaryString(aChar))   // char -> int, auto-cast
+					      .replaceAll(" ", "0")                         // zero pads
+			);
+		}
+		return String.join(
+				" ",
+				result
+		);
+	}
+
+	public void encodeAndSend(String string) {
+		String encode = encode(string);
+		send(encode);
+	}
+
+}
