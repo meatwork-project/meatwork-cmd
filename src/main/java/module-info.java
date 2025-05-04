7045a30c94eb038d23a085104049b9dfae3bfa62
dbarb <d.barbarin@hotmail.fr>
2025-05-04 17:45:42 +0200

    Update module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,7 @@
  * All rights reserved.
  */
 module com.meatwork.cmd {
-
 	requires com.meatwork.core;
 	requires info.picocli;
-	requires org.slf4j;
-	requires jakarta.inject;
-
 	exports com.meatwork.cmd.api;
 }
